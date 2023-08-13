e5cc3b32ca8cee9774cdb171b59ecb161915eafd
Owen Stubbs <owen77stubbs@gmail.com>
2023-08-13 23:26:29 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { resolve } from 'path'
 export default defineConfig({
   test: {
     setupFiles: ['dotenv/config'],
-    slowTestThreshold: 60,
+    slowTestThreshold: 20,
     globals: true,
     bail: 5,
   },
